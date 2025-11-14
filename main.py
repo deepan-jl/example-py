06766b95e282676a1909bd50dfaa852485be7008
deepan-jl <deepan.jl12345@gmail.com>
2025-11-14 07:13:22 +0000

    hi 3

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-print("hello")
+from flask import flask
+app = Flask(__name__)
+@app.route('/')
+def home():
+    return 'Hello'
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8080)
+    
